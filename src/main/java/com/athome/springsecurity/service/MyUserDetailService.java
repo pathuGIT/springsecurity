9c4cf0c79f3d016c0f9465580e4ba260d5470b8a
pathuGIT <wannigelakshan@gmail.com>
2025-07-25 16:11:33 +0530

    multiple users logging to system through db

---
+++ src/main/java/com/athome/springsecurity/service/MyUserDetailService.java
@@ -0,0 +1,28 @@
+package com.athome.springsecurity.service;
+
+import com.athome.springsecurity.model.User;
+import com.athome.springsecurity.model.UserPrinciples;
+import com.athome.springsecurity.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+
+        if(user == null) {
+            System.out.println("User Not Found");
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserPrinciples(user);
+    }
+}
